ccf1f386ac6d908a8ead0dc87b6362f5abab6da9
livesub <livesub@naver.com>
2021-01-11 13:41:05 +0900

    first

---
+++ portal/adm/admin.menu600.php
@@ -0,0 +1,12 @@
+<?php
+$menu['menu600'] = array (
+    array('600000', '게시판 관리', G5_ADMIN_URL.'/bbs/board.php?bo_table=notice', 'notice'),
+    array('600100', '협회 공지사항게시판', G5_ADMIN_URL.'/bbs/board.php?bo_table=notice', 'notice'),
+    array('600200', '협회 자유게시판', G5_ADMIN_URL.'/bbs/board.php?bo_table=free', 'free'),
+    array('600300', '협회 gallery게시판', G5_ADMIN_URL.'/bbs/board.php?bo_table=gallery', 'gallery'),
+
+    array('600400', '교육센터 공지사항게시판', G5_ADMIN_URL.'/bbs/board.php?bo_table=edu_notice', 'edu_notice'),
+    array('600500', '교육센터 게시판', G5_ADMIN_URL.'/bbs/board.php?bo_table=edu_free', 'edu_free'),
+    array('600600', '교육센터 영상자료실', G5_ADMIN_URL.'/bbs/board.php?bo_table=edu_video', 'edu_video'),
+    array('600700', '교육센터 일반자료실', G5_ADMIN_URL.'/bbs/board.php?bo_table=edu_normal', 'edu_normal'),
+);
