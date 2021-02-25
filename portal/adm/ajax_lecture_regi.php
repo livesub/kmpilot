ca1eaa0977d9e738a3c0d2595319122534973488
livesub <livesub@naver.com>
2021-02-25 14:28:45 +0900

    210225_01

--- portal/adm/ajax_lecture_regi.php
+++ portal/adm/ajax_lecture_regi.php
@@ -5,28 +5,27 @@ $html_total = $_POST['html_total'];
 $edu_onoff_type = $_POST['edu_onoff_type'];
 $edu_idx = $_POST['edu_idx'];
 $edu_type = $_POST['edu_type'];
+$process_cnt = $_POST['process_cnt'];
+$process_cnt_cut = explode(",",$process_cnt);
 $lecture_del_type = $_POST['lecture_del_type'];
-$tmp = substr($lecture_del_type, 0, -1);
+$lecture_del_type = substr($lecture_del_type, 0, -1);
 
-if($lecture_del_type == ""){
+$del_type = $_POST['del_type'];
+
+if($del_type == "inup"){
     //삭제 아닐때
-    for($i = 0; $i < $html_total; $i++){
-        $lecture_idx = $_POST['lecture_idx'.$i];
-        $lecture_subject = $_POST['lecture_subject'.$i];
-        $lecture_name = $_POST['lecture_name'.$i];
-        $lecture_time = $_POST['lecture_time'.$i];
-        $lecture_youtube = $_POST['lecture_youtube'.$i];
+    for($i = 0; $i < count($process_cnt_cut); $i++){
+        $lecture_idx = $_POST['lecture_idx'.$process_cnt_cut[$i]];
+        $lecture_subject = $_POST['lecture_subject'.$process_cnt_cut[$i]];
+        $lecture_name = $_POST['lecture_name'.$process_cnt_cut[$i]];
+        $lecture_time = $_POST['lecture_time'.$process_cnt_cut[$i]];
+        $lecture_youtube = $_POST['lecture_youtube'.$process_cnt_cut[$i]];
 
-        //DB 에 저장 되어 있는지 먼저 확인
-        $row = sql_fetch(" select count(*) as cnt from kmp_pilot_lecture_list where lecture_idx = '{$lecture_idx}' and edu_type = '{$edu_type}' ");
-        $total_count = $row['cnt'];
-
-        //삭제가 아닐때
-        if($total_count == 0){
-            //insert
+        if($lecture_idx == ""){
+            //신규데이터
             $result = sql_query(" insert into kmp_pilot_lecture_list set edu_idx = '{$edu_idx}', edu_onoff_type='{$edu_onoff_type}', edu_type='{$edu_type}', lecture_subject = '{$lecture_subject}',  lecture_name = '{$lecture_name}', lecture_time = '{$lecture_time}', lecture_youtube = '{$lecture_youtube}', lecture_regi = now() ");
         }else{
-            //update
+            //저장 데이터
             $result = sql_query(" update kmp_pilot_lecture_list set edu_idx = '{$edu_idx}', edu_onoff_type='{$edu_onoff_type}', edu_type='{$edu_type}', lecture_subject = '{$lecture_subject}',  lecture_name = '{$lecture_name}', lecture_time = '{$lecture_time}', lecture_youtube = '{$lecture_youtube}' where lecture_idx = '{$lecture_idx}' and edu_type = '{$edu_type}' ");
         }
     }
@@ -38,9 +37,9 @@ if($lecture_del_type == ""){
         echo "no";
         exit;
     }
-}else if($lecture_del_type != ""){
+}else if($del_type == "del"){
     //삭제 일때(delete 대신 플러그 값 변경)
-    $cut_comma = explode(",",$tmp);
+    $cut_comma = explode(",",$lecture_del_type);
 
     for($e = 0; $e < count($cut_comma); $e++){
         $row_sql = sql_fetch(" select count(*) as cnt from kmp_pilot_lecture_list where lecture_idx = '{$cut_comma[$e]}' and edu_type = '{$edu_type}' ");
--- portal/adm/pilot_lecture_regi.php
+++ portal/adm/pilot_lecture_regi.php
@@ -15,8 +15,13 @@ $g5['title'] = "온라인 교육 - 강의 등록";
 include_once('./admin.head.php');
 
 $sql = " select * from kmp_pilot_lecture_list where edu_idx = '{$edu_idx}' and edu_onoff_type = '{$edu_onoff_type}' and edu_type = '{$edu_type}' and lecture_del_type = 'N' order by lecture_idx asc ";
+echo $sql;
 $result = sql_query($sql);
 $now_data_cnt = sql_num_rows($result);  //현재 저장된 데이터 갯수
+$process_cnt = "";
+for ($g=0; $g < $now_data_cnt; $g++) {
+    $process_cnt .= "{$g},";
+}
 ?>
 
 <div class="local_desc01 local_desc">
@@ -37,6 +42,8 @@ $now_data_cnt = sql_num_rows($result);  //현재 저장된 데이터 갯수
 <input type='hidden' name='edu_idx' id='edu_idx' value="<?=$edu_idx?>">
 <input type='hidden' name='edu_type' id='edu_type' value="<?=$edu_type?>">
 <input type='hidden' name='lecture_del_type' id='lecture_del_type' value="">
+<input type='hidden' name='process_cnt' id='process_cnt' value="">
+<input type='hidden' name='del_type' id='del_type' value="">
 
 <div id="add_div">
 
@@ -78,29 +85,37 @@ for ($r=0; $row=sql_fetch_array($result); $r++) {
 </form>
 
 <script>
-    var i = $("#html_total").val(); //DB 저장 된 갯수 빼고 증가
+    var m = $("#html_total").val(); //DB 저장 된 갯수 빼고 증가
+    console.log("aaa=====> "+m);
     var html = "";
+    var process_cnt = "<?=$process_cnt?>";
     $(document).ready(function(){
+
         $("#add_lecture").click(function(){
-            html = "<div class='tbl_frm01 tbl_wrap' id='add"+i+"'><input type='hidden' name='lecture_idx"+i+"' id='lecture_idx"+i+"' value='<?=$row[lecture_idx]?>'>";
+//alert(m);
+            //html = "<div class='tbl_frm01 tbl_wrap' id='add"+m+"'><input type='hidden' name='lecture_idx"+m+"' id='lecture_idx"+m+"' value='<?=$row[lecture_idx]?>'>";
+            html = "<div class='tbl_frm01 tbl_wrap' id='add"+m+"'>";
             html += "    <table><tr>";
             html += "    <th scope='row'><label for='lecture_subject'>강의제목<?=$sound_only ?></label></th>";
             html += "    <td  colspan='3'>";
-            html += "       <input type='text' name='lecture_subject"+i+"' value='<?=$row[lecture_subject]?>' id='lecture_subject"+i+"' required class='frm_input required' size='80' maxlength='250'>";
+            html += "       <input type='text' name='lecture_subject"+m+"' value='<?=$row[lecture_subject]?>' id='lecture_subject"+m+"' required class='frm_input required' size='80' maxlength='250'>";
             html += "    </td>";
-            html += "    <th scope='row' rowspan='3' style='width:10px;'><input type='checkbox' id='chk"+i+"' name='chk' value='"+i+"'></th>";
+            html += "    <th scope='row' rowspan='3' style='width:10px;'><input type='checkbox' id='chk"+m+"' name='chk' value='"+m+"'></th>";
             html += "        </tr><tr>";
             html += "        <th scope='row'><label for='lecture_name'>강사명<strong class='sound_only'>필수</strong></label></th>";
-            html += "        <td><input type='text' id='lecture_name"+i+"' name='lecture_name"+i+"' value='<?=$row[lecture_name] ?>' required class='required frm_input' size='15' maxlength='50'></td>";
+            html += "        <td><input type='text' id='lecture_name"+m+"' name='lecture_name"+m+"' value='<?=$row[lecture_name] ?>' required class='required frm_input' size='15' maxlength='50'></td>";
             html += "        <th scope='row'><label for='lecture_time'>강의시간<strong class='sound_only'>필수</strong></label></th>";
-            html += "        <td><input type='text' id='lecture_time"+i+"' name='lecture_time"+i+"' value='<?=$row[lecture_time] ?>' required class='required frm_input' size='3' maxlength='3' onkeypress='inNumber();'></td>";
+            html += "        <td><input type='text' id='lecture_time"+m+"' name='lecture_time"+m+"' value='<?=$row[lecture_time] ?>' required class='required frm_input' size='3' maxlength='3' onkeypress='inNumber();'></td>";
             html += "</tr>";
             html += "        <tr><th scope='row'><label for='lecture_youtube'>유튜브주소<strong class='sound_only'>필수</strong></label></th>";
-            html += "        <td colspan='3'><input type='text' id='lecture_youtube"+i+"' name='lecture_youtube"+i+"' value='<?php echo $row[lecture_youtube] ?>' required class='required frm_input' size='80' maxlength='250'></td>";
+            html += "        <td colspan='3'><input type='text' id='lecture_youtube"+m+"' name='lecture_youtube"+m+"' value='<?php echo $row[lecture_youtube] ?>' required class='required frm_input' size='80' maxlength='250'></td>";
             html += "        </tr></table><br></div>";
 
+            process_cnt += m + ",";
+//alert("addprocess_cnt===> "+process_cnt);
+console.log("m====> "+m);
+            m++;
             $("#add_div").append(html)
-            i++;
         });
     })
 
@@ -117,57 +132,60 @@ for ($r=0; $row=sql_fetch_array($result); $r++) {
                     var str = $(this).attr("value");
                     lecture_del_type += $("#lecture_idx"+str).val()+",";
                     $("#lecture_del_type").val(lecture_del_type);
+                    process_cnt = process_cnt.replace(str+",", "");
+//alert("process_cnt   ====> " +process_cnt);
                     $("#add"+str).remove();
                 });
             }else{
                 return false;
             }
         }
+        $("#del_type").val("del");
         ajax_process(); //호출
     }
-</script>
 
-<script>
     function ajax_save_lecture(){
         var chk_len = $("input[name='chk']").length;
-        var html_total = $("#html_total").val(chk_len);
-        $("#lecture_del_type").val("");
 
         if(chk_len == 0){
             alert("강의를 추가 하세요.");
             return false;
         }else{
-            for(k = 0; k < chk_len; k++){
-                if($("#lecture_subject"+k).val() == ""){
+            process_cnt_tmp = process_cnt.substr(0,process_cnt.length-1);
+            var process_cnt_cut_array = process_cnt_tmp.split(",");
+
+            for(k = 0; k < process_cnt_cut_array.length; k++){
+                if($("#lecture_subject"+process_cnt_cut_array[k]).val() == ""){
                     alert("강의제목을 입력 하세요.");
-                    $("#lecture_subject"+k).focus();
+                    $("#lecture_subject"+process_cnt_cut_array[k]).focus();
                     return false;
                 }
 
-                if($("#lecture_name"+k).val() == ""){
+                if($("#lecture_name"+process_cnt_cut_array[k]).val() == ""){
                     alert("강사명을 입력 하세요.");
-                    $("#lecture_name"+k).focus();
+                    $("#lecture_name"+process_cnt_cut_array[k]).focus();
                     return false;
                 }
 
-                if($("#lecture_time"+k).val() == ""){
+                if($("#lecture_time"+process_cnt_cut_array[k]).val() == ""){
                     alert("강의시간을 입력 하세요.");
-                    $("#lecture_time"+k).focus();
+                    $("#lecture_time"+process_cnt_cut_array[k]).focus();
                     return false;
                 }
 
-                if($("#lecture_youtube"+k).val() == ""){
+                if($("#lecture_youtube"+process_cnt_cut_array[k]).val() == ""){
                     alert("유튜브주소를 입력 하세요.");
-                    $("#lecture_youtube"+k).focus();
+                    $("#lecture_youtube"+process_cnt_cut_array[k]).focus();
                     return false;
                 }
             }
+
+            $("#process_cnt").val(process_cnt_cut_array);
+            $("#del_type").val("inup");
             ajax_process(); //호출
         }
     }
-</script>
 
-<script>
     function ajax_process(){
         var ajaxUrl = "ajax_lecture_regi.php";
         var queryString = $("form[name=flecture]").serialize() ;
@@ -196,6 +214,10 @@ for ($r=0; $row=sql_fetch_array($result); $r++) {
         });
     }
 </script>
+
+
+
+
 
 <?php
 run_event('admin_member_form_after', $mb, $w);
