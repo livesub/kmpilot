0fc08c7c646ab7d8bece91463cea4ff453bf8d14
livesub <livesub@naver.com>
2021-03-05 14:59:22 +0900

    210305_02

--- edu/data/cache/content-pilot_edu_list-a98127.php
+++ edu/data/cache/content-pilot_edu_list-a98127.php
@@ -1,3 +1,3 @@
 <?php if (!defined('_GNUBOARD_')) exit; ?>
 
-YTozOntzOjQ6InRpbWUiO2k6MTYxNDczOTgzMDtzOjM6InR0bCI7aToxMDgwMDtzOjQ6ImRhdGEiO047fQ==
+YTozOntzOjQ6InRpbWUiO2k6MTYxNDkwNjU4NztzOjM6InR0bCI7aToxMDgwMDtzOjQ6ImRhdGEiO047fQ==
--- edu/data/cache/content-pilot_mypage-a98127.php
+++ edu/data/cache/content-pilot_mypage-a98127.php
@@ -1,3 +1,3 @@
 <?php if (!defined('_GNUBOARD_')) exit; ?>
 
-YTozOntzOjQ6InRpbWUiO2k6MTYxMzk4MzEyNTtzOjM6InR0bCI7aToxMDgwMDtzOjQ6ImRhdGEiO047fQ==
+YTozOntzOjQ6InRpbWUiO2k6MTYxNDkwNjUwNjtzOjM6InR0bCI7aToxMDgwMDtzOjQ6ImRhdGEiO047fQ==
