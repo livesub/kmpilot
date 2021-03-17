891963a22f4f8168e000ff91d4ab61c06fd99a0a
livesub <livesub@naver.com>
2021-03-17 10:31:36 +0900

    aaaaa

--- portal/skin/board/basic/view.skin.php
+++ portal/skin/board/basic/view.skin.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
-
+//테스트입니다.
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0);
 ?>
